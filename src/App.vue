9ac94a030993d46fbead00d270818456a9802658
Dion Jones <monkey@deadcatdesigns.co.uk>
2018-05-10 11:23:42 +0100

    initial commit: Working call to ticker endpoint

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <SummaryDollarValue></SummaryDollarValue>
+    {{cryptoListing.data}}
+  </div>
+</template>
+
+<script>
+import SummaryDollarValue from './components/SummaryDollarValue';
+import axios from 'axios';
+const API_KEY = 'AIzaSyAKV4Nn-zOThQqqnvXPCghTHqiO_zYNKfw';
+
+export default {
+  name: 'App',
+  components: {
+    SummaryDollarValue: SummaryDollarValue
+  },
+  data() {
+    return { cryptoListing: [] };
+  },
+  methods: {},
+  mounted: function() {
+    axios
+      .get('https://api.coinmarketcap.com/v2/ticker/', {
+        params: {}
+      })
+      .then(res => {
+        this.cryptoListing = res.data;
+      });
+  }
+};
+</script>
